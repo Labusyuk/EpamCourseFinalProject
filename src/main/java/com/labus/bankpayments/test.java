8994d57345ee5cdbf8e15bcb2b64863cc7208dea
Labusyuk <labusyuk@gmail.com>
2019-01-25 08:37:22 +0200

    Git: master

--- src/main/java/com/labus/bankpayments/test.java
+++
@@ -1,9 +0,0 @@
-package com.labus.bankpayments;
-
-import com.labus.bankpayments.db.ConnectionPool;
-
-public class test {
-    public static void main(String[] args) {
-        ConnectionPool pool = ConnectionPool.getInstance();
-    }
-}
